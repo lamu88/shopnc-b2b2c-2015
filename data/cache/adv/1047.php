0389ce55be1ef7c13b8bba074f94a2ab344bf0cf
lamu88 <lamuyangsheng@126.com>
2016-06-21 16:10:38 +0800

    shopnc b2b2c 2015

---
+++ data/cache/adv/1047.php
@@ -0,0 +1,14 @@
+<?php defined('InShopNC') or exit('Access Invalid!'); return array (
+  'ap_id' => '1047',
+  'ap_name' => '首页顶部广告',
+  'ap_intro' => '',
+  'ap_class' => '0',
+  'ap_display' => '2',
+  'is_use' => '1',
+  'ap_width' => '1200',
+  'ap_height' => '70',
+  'ap_price' => '0',
+  'adv_num' => '1',
+  'click_num' => '0',
+  'default_content' => '04878494946508568.jpg',
+);
